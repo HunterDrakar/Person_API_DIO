b92a032495724395e861ec72901f89d172121501
HunterDrakar <huntereoq@gmail.com>
2021-08-23 18:33:30 -0300

    Segundo commit

---
+++ src/main/java/com/one/digitalinnovation/personapi/service/PersonService.java
@@ -0,0 +1,29 @@
+package com.one.digitalinnovation.personapi.service;
+
+import com.one.digitalinnovation.personapi.dto.MessageResponseDTO;
+import com.one.digitalinnovation.personapi.entity.Person;
+import com.one.digitalinnovation.personapi.repository.PersonRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@Service
+public class PersonService {
+
+    private PersonRepository personRepository;
+
+    @Autowired
+    public PersonService(PersonRepository personRepository){
+        this.personRepository = personRepository;
+    }
+
+    @PostMapping
+    public MessageResponseDTO createPerson(Person person){
+        Person savedPerson = personRepository.save(person);
+        return MessageResponseDTO
+                .builder()
+                .message("Created person with ID" + savedPerson.getId())
+                .build();
+    }
+}
